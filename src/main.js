7d7f41a4ff622a623ceead12818eb5f415462745
Kreoin <steamuser85@yandex.ru>
2024-10-28 18:52:08 +0300

    add routes

--- src/main.js
+++ src/main.js
@@ -1,6 +1,40 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: () => import('./pages/Home.vue'),
+    },
+    {
+      path: '/professions',
+      name: 'professions',
+      component: () => import('./pages/Professions.vue'),
+    },
+    {
+      path: '/departments',
+      name: 'departments',
+      component: () => import('./pages/Departments.vue'),
+    },
+    {
+      path: '/employees',
+      name: 'employees',
+      component: () => import('./pages/Employees.vue'),
+    },
+    {
+      path: '/professions/:id',
+      name: 'profession',
+      component: () => import('./pages/Profession.vue'),
+    },
+  ],
+})
+app.use(router)
+app.mount('#app')
