1f7c3fa51d3ae64a22e58507d3e9e6a3eab3c222
Kreoin <steamuser85@yandex.ru>
2024-10-28 18:50:38 +0300

    create main template

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <header>
+      <nav>
+        <div class="space-x-6">
+          <router-link to="/" class="nav-link"> На главную </router-link>
+        </div>
+        <div class="space-x-6">
+          <router-link to="/professions" class="nav-link">
+            Профессии
+          </router-link>
+
+          <router-link to="/departments" class="nav-link"> Отделы </router-link>
+
+          <router-link to="/employees" class="nav-link">
+            Сотрудники
+          </router-link>
+        </div>
+      </nav>
+    </header>
+
+    <main class="p-4">
+      <router-view></router-view>
+    </main>
+  </div>
+</template>
